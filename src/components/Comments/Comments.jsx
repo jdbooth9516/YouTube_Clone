d18acfe66b6cae3cdd34fdc70b97013d2b113171
Jeremiah Booth <jereimah.boothphotog@gmail.com>
2021-06-24 11:37:29 -0700

    started the class for the comments

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+export default class Comments extends Component {
+    constructor(props){
+        super(props)
+        this.state ({
+            visible: false,
+            replyVisible: false, 
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
